3ebf0a8b701193fce131ac8d7154703baf1ef94e
sam <samuel.fuentes.baio@gmail.com>
2024-04-17 20:43:41 +0200

    rfefg

--- TraballoFinal/js/script.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector('.hamburger');
-const sidebar = document.querySelector('.sidebar');
-
-hamburger.addEventListener('click', () => {
-  sidebar.classList.toggle('open');
-});
